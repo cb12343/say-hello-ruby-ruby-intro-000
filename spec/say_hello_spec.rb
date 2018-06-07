705d4318f925f38780b7da16a9f841623c2d14a1
clbowling12@gmail.com <clbowling12@gmail.com>
2018-06-07 16:51:08 +0000

    automatically backed up by learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -7,9 +7,10 @@ describe "say_hello" do
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
     say_hello("Kent Beck")
   end
-
+name = "Kent Beck"
   it 'defaults to Ruby Programmer when no name is passed in' do
     expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
     say_hello()
   end
+
 end
